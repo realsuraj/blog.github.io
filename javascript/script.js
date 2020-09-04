142618fcb3026b4b9d9cb79911f09eb4723fff3c
realsuraj <66870141+realsuraj@users.noreply.github.com>
2020-09-04 14:38:39 +0530

    update 1.1

--- javascript/script.js
+++ javascript/script.js
@@ -26,8 +26,8 @@ $(document).ready(function() {
 	// owl carousel for blog
 	$('.owl-carousel').owlCarousel({
 		loop: true,
-		// autoplay: true,
-		// autoplayTimeout: 3000,
+		autoplay: true,
+		autoplayTimeout: 3000,
 		dots: false,
 		nav: true,
 		navText: [ $('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next') ],
